78230aca677796f15fd7f03026e498614ac0d0af
Ig0rj <48989834+Ig0rj@users.noreply.github.com>
2024-09-14 18:47:06 +0300

    Add files via upload

---
+++ Release/Core/Src/main.d
@@ -0,0 +1,17 @@
+Core/Src/main.o: ../Core/Src/main.cpp ../Core/Inc/main.h \
+ ../Core/Inc/uart.h ../Core/Inc/stm32l4xx.h ../Core/Inc/stm32l432xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h ../Core/Inc/system_stm32l4xx.h
+../Core/Inc/main.h:
+../Core/Inc/uart.h:
+../Core/Inc/stm32l4xx.h:
+../Core/Inc/stm32l432xx.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Core/Inc/system_stm32l4xx.h:
